34e4e18982c2c8449c03449b56e064d2cf2e5713
baimingjiang <baimingjiang@pwrd.com>
2020-05-08 10:47:33 +0800

    显示控制台

--- uieditor/src/main.lua
+++ uieditor/src/main.lua
@@ -6,7 +6,7 @@ require "cocos.init"
 
 local function main()
 
-    local breakSocketHandle, debugXpCall = require("LuaDebug")("localhost", 7005)
+    local breakSocketHandle, debugXpCall = require("LuaDebugjit")("localhost", 7005)
     cc.Director:getInstance():getScheduler():scheduleScriptFunc(breakSocketHandle, 0.3, false) 
 
     require("app.MyApp"):create():run()
